807151ec25c14ce9aa1436f585d957f2c42908e1
vijaykumar durgaprasad raj <56254022+vijaykumardraj@users.noreply.github.com>
2021-06-18 20:12:00 +0300

    Update test1.sh
    saved in github

--- test1.sh
+++ test1.sh
@@ -13,4 +13,18 @@ echo enter number 2
 read n2
 sum=$[ $n1 + $n2 ]
 echo $sum
+op1=add
+echo enter operation - add minus multiply divide
+read op1
+if [ $op1 = "add" ]
+then
+n3=$[ $n1 + $n2 ]
+echo $n3
+elif [ $op1 = "minus ]
+n3=$[ $n1 - $n2 ]
+echo $n3
+else
+echo you entered other operation
+fi
+
 
