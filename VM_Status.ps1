5effdb9b720691b00a6430d992c3c0e0e6ca6351
vijaykumar durgaprasad raj <vijaykumar.raj@hotmail.com>
2021-07-11 17:31:57 +0300

    Create VM_Status.ps1

---
+++ VM_Status.ps1
@@ -0,0 +1,19 @@
+﻿echo "Status of VM on each nodes"
+$i=1
+while( $i -le 13 )
+{
+    if( $i -le 9 )
+    {
+    echo "**argdchv0$i"
+    echo "***********"
+    get-vm -ComputerName argdchv0$i
+    }
+    else
+    {
+    echo "**argdchv$i"
+    echo "***********"
+    get-vm -ComputerName argdchv$i
+    }
+    $i++
+}
+
