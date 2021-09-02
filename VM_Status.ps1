ccc2de90c4bbf98dc09cf0bd5ef7a556271282f2
vijaykumar durgaprasad raj <vijaykumar.raj@hotmail.com>
2021-09-02 16:37:41 +0300

    save

--- VM_Status.ps1
+++ VM_Status.ps1
@@ -1,6 +1,13 @@
 ﻿#Invoke-Command -ComputerName PTYlkitb28 {get-vm | measure-vm }
 #get-vm | Enable-VMResourceMetering
 #get-vm -ComputerName PTYdchv13 | Measure-VM
+try {write-host "in try"
+throw {write-host "this in try after error caught"
+write-host "try....."}
+}
+catch{ throw }
+
+finally {
 echo "Status of VM on each nodes"
 $i=1
 while( $i -le 13 )
@@ -9,14 +16,15 @@ while( $i -le 13 )
     {
     echo "**PTYdchv0$i"
     echo "***********"
-    get-vm -ComputerName PTYdchv0$i
+    #get-vm -ComputerName PTYdchv0$i
     }
     else
     {
     echo "**PTYdchv$i"
     echo "***********"
-    get-vm -ComputerName PTYdchv$i
+    #get-vm -ComputerName PTYdchv$i
     }
     $i++
 }
 
+}
